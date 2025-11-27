7509b6b494ad51c10b7317d3838c62146ba9b7a8
Sayed Anowar <anowarsayed3@bisicsl.org>
2025-11-27 08:00:21 +0000

    init

---
+++ tests/firstTest.spec.js
@@ -0,0 +1,16 @@
+import {expect, test} from "@playwright/test";
+
+test('first test', async ({page}) => {
+    await page.goto('https://demo.akveo.com/ngx-admin/pages/dashboard');
+    await page.screenshot({path: 'screenshots/firstTest.png'});
+
+    await page.getByText('Material Light').click();
+    // await expect(page).toHaveTitle('dashboard');
+
+    await page.getByText('Forms').click();
+    await page.screenshot({path: 'screenshots/firstTest2.png'});
+
+    await page.getByText('Form Layouts').click();
+
+    await page.close();
+})
